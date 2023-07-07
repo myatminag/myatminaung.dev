ff51a68cc1e5cdfe6e5086f9e3538203b4fe2e9b
MyatMinAung <myatminaung.dev@gmail.com>
2023-07-07 23:17:49 +0630

    update: font size at experience page

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -13,27 +13,27 @@ const Experience = () => {
                     <Text fontSize="xs">January 2023 - Present</Text>
                 </Box>
                 <UnorderedList>
-                    <ListItem mb={3}>
+                    <ListItem fontSize={{ lg: "sm" }} mb={3}>
                         Collaborated with cross-functional teams, including
                         designers, testers, and backend developers, to deliver
                         high-quality features on tight timelines.
                     </ListItem>
-                    <ListItem mb={3}>
+                    <ListItem fontSize={{ lg: "sm" }} mb={3}>
                         Developed and maintained micro-frontends using React,
                         Next and state management libraries like Redux and
                         Zustand.
                     </ListItem>
-                    <ListItem mb={3}>
+                    <ListItem fontSize={{ lg: "sm" }} mb={3}>
                         Implemented responsive design strategies and
                         mobile-first development approach to ensure seamless
                         user experiences across all devices.
                     </ListItem>
-                    <ListItem mb={3}>
+                    <ListItem fontSize={{ lg: "sm" }} mb={3}>
                         Worked alongside with five front-end developers for
                         developing eCommerce service platforms for variety of
                         seller, retailer, and vendor.
                     </ListItem>
-                    <ListItem>
+                    <ListItem fontSize={{ lg: "sm" }}>
                         Implemented and shipped the front-end of Aya Zay
                         multi-seller web app for Aya Bank which let users can
                         buy various kinds of products at Aya Pay application.
@@ -49,26 +49,26 @@ const Experience = () => {
                     <Text fontSize="xs">November 2022 - Present</Text>
                 </Box>
                 <UnorderedList>
-                    <ListItem mb={3}>
+                    <ListItem fontSize={{ lg: "sm" }} mb={3}>
                         Collaborated with a team of developers and designers to
                         create responsive and user-friendly web applications.
                     </ListItem>
-                    <ListItem mb={3}>
+                    <ListItem fontSize={{ lg: "sm" }} mb={3}>
                         Implemented front-end frameworks such as React and
                         Nextjs to enhance the user interface and improve overall
                         user experience.
                     </ListItem>
-                    <ListItem mb={3}>
+                    <ListItem fontSize={{ lg: "sm" }} mb={3}>
                         Utilized server-side programming languages like Node.js
                         and Express.js to build robust and scalable back-end
                         functionality.
                     </ListItem>
-                    <ListItem mb={3}>
+                    <ListItem fontSize={{ lg: "sm" }} mb={3}>
                         Collaborated closely with team members to understand
                         project requirements, provide feedback, and deliver
                         non-profit projects.
                     </ListItem>
-                    <ListItem>
+                    <ListItem fontSize={{ lg: "sm" }}>
                         Kept up-to-date with the latest technologies, industry
                         trends, and best practices through continuous learning
                         and self-improvement.
