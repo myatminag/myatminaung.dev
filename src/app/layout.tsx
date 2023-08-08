bc060895d5472c68fa753fc87406e517d9e96fbb
MyatMinAung <myatminaung.dev@gmail.com>
2023-08-08 21:59:28 +0630

    migrate: chakra ui to tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
+import { Inter } from "next/font/google";
+import LocalFont from "next/font/local";
 import "./globals.css";
-import { ChakraWrapper } from "@/provider/chakra.provider";
 
 export const metadata = {
     title: "Myat Min Aung | A Full Stack Web Developer",
@@ -7,16 +8,27 @@ export const metadata = {
         "Hello, I am Myat Min Aung. I am a full-stack web developer from Yangon, Myanmar. Specializing in mern-stack development, I am using Reactjs & Nextjs on fontend development and Expressjs & Nodejs on backend development. I have a serious passion for developing web applications, dynamic user experiences and creating intuitive.",
 };
 
+const inter = Inter({
+    subsets: ["latin"],
+    variable: "--font-inter",
+});
+
+const calSans = LocalFont({
+    src: "../../public/font/CalSans-SemiBold.ttf",
+    variable: "--font-calsans",
+});
+
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
-            <body>
-                <ChakraWrapper>{children}</ChakraWrapper>
-            </body>
+        <html
+            lang="en"
+            className={[inter.variable, calSans.variable].join(" ")}
+        >
+            <body className="bg-black">{children}</body>
         </html>
     );
 }
