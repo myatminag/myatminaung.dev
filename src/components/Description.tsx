44cdf37bd15bf6747e4981503abff5eec274ac5b
MyatMinAung <myatminaung.dev@gmail.com>
2023-08-10 22:47:39 +0630

    fix: spelling

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -8,8 +8,8 @@ const Description = () => {
                 I&apos;m a fullstack web developer based in Yangon, Myanmar.
             </p>
             <p className="leading-7 text-zinc-400">
-                Interested in Javascript & Typescript, I have experience in
-                implementing front-end frameworks like React.js, and Next.js. In
+                Interested in JavaScript & TypeScript, I have experience in
+                implementing frontend frameworks like React.js, and Next.js. In
                 backend development, I have hands-on experience in Node.js,
                 Express.js, and MongoDB to build robust and scalable web
                 applications.
