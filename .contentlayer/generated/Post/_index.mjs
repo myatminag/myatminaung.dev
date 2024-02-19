a60df9611c9076dc73c0cfc9ecd201c1b951c73f
MyatMinAung <myatminaung.dev@gmail.com>
2024-02-19 15:02:34 +0630

    :speech_balloon: add: unary operator

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -3,5 +3,6 @@
 import arithmeticOperatorsMdx from './arithmetic-operators.mdx.json' assert { type: 'json' }
 import functionExpressionVsFunctionDeclarationMdx from './function-expression-vs-function-declaration.mdx.json' assert { type: 'json' }
 import implementingUrlSearchParamsMdx from './implementing-url-search-params.mdx.json' assert { type: 'json' }
+import unaryOperatorMdx from './unary-operator.mdx.json' assert { type: 'json' }
 
-export const allPosts = [arithmeticOperatorsMdx, functionExpressionVsFunctionDeclarationMdx, implementingUrlSearchParamsMdx]
+export const allPosts = [arithmeticOperatorsMdx, functionExpressionVsFunctionDeclarationMdx, implementingUrlSearchParamsMdx, unaryOperatorMdx]
